91c4bfc72f352753f1951f9bece0b59bef8b7d22
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-02-17 19:25:37 -1000

    rename folders/files to match spec criteria

---
+++ js/shopping_list.js
@@ -0,0 +1,36 @@
+class ShoppingList {
+  constructor() {
+    this.items = [];
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    if (item) {
+      const index = this.items.map(i => i.name).indexOf(item);
+      console.log("index: ", index);
+      if (index === -1) {
+        throw new TypeError("Item does not exist!");
+      } else if (index === 0) {
+        this.items.shift();
+      } else {
+        this.items.splice(index - 1, 1);
+      }
+    } else {
+      if (this.items.length >= 1) {
+        this.items.pop();
+      } else {
+        this.items = [];
+      }
+    }
+    return this.items;
+  }
+
+  render() {
+    const itemsArr = this.items.map(i => i.render());
+    console.log(this.items.map(i => i.render()));
+    return `<ul>${itemsArr}</ul>`;
+  }
+}
--- js/shopping_list_test.js
+++ js/shopping_list_test.js
@@ -1,58 +1,209 @@
-class ShoppingListItem {
-  constructor(name, description) {
-    this.name = name;
-    this.description = description;
-    this.is_done = false;
-  }
+const should = chai.should();
+const expect = chai.expect;
+const assert = chai.assert;
 
-  check() {
-    this.is_done = true;
-  }
+describe("ShoppingListItem", () => {
+  it("should be a function", () => {
+    expect(ShoppingListItem).to.be.a("function");
+  });
 
-  uncheck() {
-    this.is_done = false;
-  }
+  describe("#name", () => {
+    it("ShoppingListItem should have a property named 'name'", () => {
+      const myShoppingListItem = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      myShoppingListItem.name.should.equal("Avocado");
+    });
+  });
 
-  render() {
-    return `<li class="completed_${this.is_done}"><span>${
-      this.name
-    }</span><span>${this.description}</span></li>`;
-  }
-}
+  describe("#description", () => {
+    it("ShoppingListItem should have a property named 'description'", () => {
+      const myShoppingListItem = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      myShoppingListItem.description.should.equal("Must be eaten immediately.");
+    });
+  });
 
-class ShoppingList {
-  constructor() {
-    this.items = [];
-  }
+  describe("#is_done", () => {
+    it("ShoppingListItem should have a property named 'is_done'", () => {
+      const myShoppingListItem = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      expect(myShoppingListItem.is_done).to.be.false;
+    });
+  });
 
-  addItem(item) {
-    this.items.push(item);
-  }
+  describe(".check", () => {
+    it("ShoppingListItem should have a method named 'check'", () => {
+      const myShoppingListItem = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      expect(myShoppingListItem.check).to.be.a("function");
+    });
+    it("Calling the instance's check method should set it's 'is_done' property to true", () => {
+      const myShoppingListItem = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      myShoppingListItem.check();
+      myShoppingListItem.is_done.should.equal(true);
+    });
+  });
 
-  removeItem(item) {
-    if (item) {
-      const index = this.items.map(i => i.name).indexOf(item);
-      console.log("index: ", index);
-      if (index === -1) {
-        throw new TypeError("Item does not exist!");
-      } else if (index === 0) {
-        this.items.shift();
-      } else {
-        this.items.splice(index - 1, 1);
-      }
-    } else {
-      if (this.items.length >= 1) {
-        this.items.pop();
-      } else {
-        this.items = [];
-      }
-    }
-    return this.items;
-  }
+  describe(".uncheck", () => {
+    it("ShoppingListItem should have a method named 'uncheck'", () => {
+      const myShoppingListItem = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      expect(myShoppingListItem.uncheck).to.be.a("function");
+    });
+    it("Calling the instance's uncheck method should set it's 'is_done' property to false", () => {
+      const myShoppingListItem = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      myShoppingListItem.uncheck();
+      myShoppingListItem.is_done.should.equal(false);
+    });
+  });
 
-  render() {
-    const itemsArr = this.items.map(i => i.render());
-    console.log(this.items.map(i => i.render()));
-    return `<ul>${itemsArr}</ul>`;
-  }
-}
+  describe(".render", () => {
+    it("ShoppingListItem should have a method named 'render'", () => {
+      const myShoppingListItem = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      expect(myShoppingListItem.render).to.be.a("function");
+    });
+    it("Calling the instance's render method will construct and return an html formatted string.", () => {
+      const myShoppingListItem = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      const avocadoRender = myShoppingListItem.render();
+      expect(avocadoRender).to.be.a("string");
+    });
+  });
+});
+
+describe("ShoppingList", () => {
+  it("should be a function", () => {
+    expect(ShoppingList).to.be.a("function");
+  });
+
+  describe("#items", () => {
+    it("ShoppingList should have a property named 'items'", () => {
+      const myShoppingList = new ShoppingList();
+      expect(myShoppingList.items).to.be.an("Array");
+    });
+    it("should be an array", () => {
+      const myShoppingList = new ShoppingList();
+      myShoppingList.items.should.deep.equal([]);
+    });
+  });
+
+  describe(".addItem", () => {
+    it("ShoppingList should have a method named 'addItem'", () => {
+      const myShoppingList = new ShoppingList();
+      expect(myShoppingList.addItem).to.be.a("function");
+    });
+    it("it should add an item (by passing in a ShoppingListItem object) to the items array", () => {
+      const myShoppingList = new ShoppingList();
+      const avocado = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      myShoppingList.addItem(avocado);
+      myShoppingList.items.should.contain(avocado);
+    });
+  });
+
+  describe(".removeItem", () => {
+    it("ShoppingList should have a method named 'removeItem'", () => {
+      const myShoppingList = new ShoppingList();
+      expect(myShoppingList.removeItem).to.be.a("function");
+    });
+    it("Invoking the removeItem method by passing in a ShoppingListItem object (that exists in the items array) should remove that object from the items array", () => {
+      const myShoppingList = new ShoppingList();
+      const avocado = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      const chips = new ShoppingListItem("chips", "Store in a cool dry place.");
+      myShoppingList.addItem(avocado);
+      myShoppingList.addItem(chips);
+      myShoppingList.removeItem("Avocado");
+      myShoppingList.items.should.contain(chips);
+      expect(myShoppingList.items.length).to.equal(1);
+    });
+    it("Invoking the removeItem method with no parameters should remove the last item in the items list, if there are any itmes, else it does nothing", () => {
+      const myShoppingList = new ShoppingList();
+      const avocado = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      const chips = new ShoppingListItem("chips", "Store in a cool dry place.");
+      myShoppingList.addItem(avocado);
+      myShoppingList.addItem(chips);
+      myShoppingList.removeItem();
+      expect(myShoppingList.items.length).to.equal(1);
+
+      myShoppingList.removeItem();
+      myShoppingList.removeItem();
+      expect(myShoppingList.items).to.deep.equal([]);
+    });
+    it("Invoking the removeItem method by passing in anything else that is not a ShoppingListItem object (that exists in the items array) should immediately throw an error", () => {
+      const myShoppingList = new ShoppingList();
+      expect(function() {
+        myShoppingList.removeItem("Avocado");
+      }).to.throw(TypeError, "Item does not exist!");
+    });
+  });
+
+  describe(".render", () => {
+    it("ShoppingList should have a method named 'render'", () => {
+      const myShoppingList = new ShoppingList();
+      expect(myShoppingList.render).to.be.a("function");
+    });
+    it("Calling the instance's render method will concatenate the result of calling render() on each item in this object's items array...", () => {
+      const myShoppingList = new ShoppingList();
+      const avocado = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      const chips = new ShoppingListItem("chips", "Store in a cool dry place.");
+      myShoppingList.addItem(avocado);
+      myShoppingList.addItem(chips);
+      console.log("render: ", myShoppingList.render());
+      expect(myShoppingList.render()).to.deep.include("</li>");
+    });
+    it("the results should be wrapped in <ul> tags", () => {
+      const myShoppingList = new ShoppingList();
+      const avocado = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      const chips = new ShoppingListItem("chips", "Store in a cool dry place.");
+      myShoppingList.addItem(avocado);
+      myShoppingList.addItem(chips);
+      expect(myShoppingList.render()).to.deep.include("<ul>");
+    });
+    it("the results should return an html formatted string", () => {
+      const myShoppingList = new ShoppingList();
+      const avocado = new ShoppingListItem(
+        "Avocado",
+        "Must be eaten immediately."
+      );
+      const chips = new ShoppingListItem("chips", "Store in a cool dry place.");
+      myShoppingList.addItem(avocado);
+      myShoppingList.addItem(chips);
+      expect(myShoppingList.render()).to.be.a("string");
+    });
+  });
+});
