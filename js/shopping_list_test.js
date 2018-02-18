8a8210133ac066e3fd329e5eb40037171dd52c01
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-02-17 16:37:37 -1000

    ShoppingListItem tests pass

---
+++ js/shopping_list_test.js
@@ -0,0 +1,24 @@
+class ShoppingListItem {
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+    this.is_done = false;
+  }
+
+  check() {
+    this.is_done = true;
+  }
+
+  uncheck() {
+    this.is_done = false;
+  }
+
+  render() {
+    return `<li class="completed_${this.is_done}"><span>${
+      this.name
+    }</span><span>${this.description}</span></li>`;
+  }
+}
+
+let avocado = new ShoppingListItem("avocado", "eat now!");
+console.log(avocado.render());
